642d5f725da09e7ad11cda30ba9d9d4b790400ea
unknown <cesmartinez@uniboyaca.edu.co>
2023-05-18 17:21:50 -0500

    Programación de los botones registrar, modificar y eliminar

--- js/gestionUsuarios.js
+++ js/gestionUsuarios.js
@@ -13,10 +13,10 @@ $(document).ready(function(){
            html+='      <td>' + usuario.name+'</td>';
            html+='      <td>'+ usuario.username+'</td>';
            html+='      <td>';
-           html+='          <button name="modificar">Modificar</button>';
+           html+='          <button class="btnModificar" data-id="' + usuario.id + '" >Modificar</button>';
            html+='      </td>';
            html+='      <td>';
-           html+='          <button name="eliminar">Eliminar</button>';
+           html+='          <button class="btnEliminar" data-id="' + usuario.id + '" >Eliminar</button>';
            html+='      </td>';
            html+='</tr>';
             
@@ -26,61 +26,130 @@ $(document).ready(function(){
         console.error(error);
     });
 
-    /*
-    * Funcionalidad para registrar un usuario
-    */
+    let condicionGuardar = 0;
+    let id = null;
 
-    $.ajax({
-        url: 'http://localhost:8000/usuarios',
-        method: 'post',
-        data:{
-            name: 'Admin 1',
-            username: 'admin 1',
-            password: '3210'
+    // REGISTRAR USUARIOS
+
+    $(document).on("click", "#btnRegistrar", function(){
+
+        document.getElementById('titulo').innerText = 'Registrar';
+        condicionGuardar = 1;
+        clean();
+
+    });
+
+    // MODIFICAR REGISTRO
+
+    $(document).on("click", ".btnModificar", function(){
+
+        document.getElementById('titulo').innerText = 'Modificar';
+
+        condicionGuardar = 2;
+        id = $(this).data("id"); //Guarda en una variable el id recogido en las propiedades del boton Modificar
+        var name = $(this).closest("tr").find("td:eq(0)").text(); //TR = Fila, TD = celda
+        var username = $(this).closest("tr").find("td:eq(1)").text();
+
+        $("#nameId").val(name);
+        $("#usernameId").val(username);
+
+    });
+
+    // ELIMINAR REGISTRO
+
+    $(document).on("click", ".btnEliminar", function(){
+
+        id = $(this).data("id");
+
+        $.ajax({
+            url: 'http://localhost:8000/usuarios/' + id,
+            method: 'delete',
+        }).done(response=>{
+            const dataJson = JSON.parse(response);
+            const msg = dataJson.data; 
+            alert(msg);
+
+            location.reload();
+
+        }).fail(error=>{
+            const dataJson = JSON.parse(response);
+            const msg = dataJson.data; 
+            alert(msg);
+        });
+
+    });
+
+    //FUNCIONALIDAD BOTON GUARDAR
+
+    document.getElementById('btnGuardar').addEventListener('click', guardar);
+
+    function guardar(){
+
+        //Variables a utilizar en el ajax
+        let name = document.getElementById('nameId');
+        let userName= document.getElementById('usernameId');
+        let password = document.getElementById('passwordId');
+
+
+        //Variables para validar que los input no puedan estar vacios
+        let nameVer = document.getElementById('nameId').value;
+        let userNameVer= document.getElementById('usernameId').value;
+        let passwordVer = document.getElementById('passwordId').value;
+
+
+        if(condicionGuardar == 0){
+            alert("Debe seleccionar una funcionalidad para el botón");
+            return;
+        }else if(nameVer.trim() === '' || userNameVer.trim() === '' || passwordVer.trim() === ''){
+            alert ("No pueden haber campos vacios");
+            return
         }
-    }).done(response=>{
-        const dataJson = JSON.parse(response);
-        const msg = dataJson.data; 
-        alert(msg);
-    });
 
-    $.ajax({
-        url: 'http://localhost:8000/usuarios/2',
-        method: 'put',
-        data:{
-            name: 'Admin 1',
-            username: 'admin 1',
-            password: '3210'
+        if(condicionGuardar == 1){
+            $.ajax({
+                url: 'http://localhost:8000/usuarios',
+                method: 'post',
+                data: {
+                    name: name.value,
+                    username: userName.value,
+                    password: password.value
+                }
+            }).done(response => {
+                const dataJson = JSON.parse(response);
+                const msg = dataJson.data;
+                alert(msg)
+                location.reload();
+
+            });
+        }else if(condicionGuardar == 2){
+            $.ajax({
+                url: 'http://localhost:8000/usuarios/' + id,
+                method: 'put',
+                data:{
+                         name: name.value,
+                         username: userName.value,
+                         password: password.value
+                     }
+                 }).done(response=>{
+                     const dataJson = JSON.parse(response);
+                     const msg = dataJson.data; 
+                     alert(msg);
+                    location.reload();
+                     
+                 });
         }
-    }).done(response=>{
-        const dataJson = JSON.parse(response);
-        const msg = dataJson.data; 
-        alert(msg);
-    });
 
-    $.ajax({
-        url: 'http://localhost:8000/usuarios/7',
-        method: 'delete',
-    }).done(response=>{
-        const dataJson = JSON.parse(response);
-        const msg = dataJson.data; 
-        alert(msg);
-    }).fail(error=>{
-        const dataJson = JSON.parse(response);
-        const msg = dataJson.data; 
-        alert(msg);
-    });
+    }
 
-    $.ajax({
-        url: 'http://localhost:8000/usuarios/2',
-        method: 'get',
-    }).done(response=>{
-        const dataJson = JSON.parse(response);
-        const usuario = dataJson.data; 
-        console.log(usuario);
-    });
+    function clean(){
+        let name = "";
+        let userName= "";
+        let password = "";
 
-    //Eliminar los usuarios
+        document.getElementById("nameId").value = name;
+        document.getElementById("usernameId").value = userName;
+        document.getElementById("passwordId").value = password;
 
-    
+    };
+
   });
